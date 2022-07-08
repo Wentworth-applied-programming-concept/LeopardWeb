b96abb597ca023dd1e08be4131f8177893ab7613
Danastor <danastora@wit.edu>
2022-07-08 10:56:15 -0400

    Gui work

--- Classes/Instructor.py
+++ Classes/Instructor.py
@@ -4,6 +4,7 @@ class Instructor(User):
     # constructor
     def __init__(self, firstName, lastName, ID):
        super().__init__(firstName,lastName, ID)
+       self.courseRoster= []
 
     def printSchedule(self):
         print(f"Prof. {self.lastName} is printing his/her schedule....")
--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from unittest import result
 from dbConnection import *
 from Classes.User import User
 from Classes.Admin import Admin
@@ -6,15 +7,14 @@ from Classes.Student import Student
 
 from tkinter import *
 from tkinter import messagebox
+from tkinter import ttk
 
-#---Password----
-# student = pass1235
-# instr = pass1234
-# admin = pass123
 
 
 def Dummy():
     pass
+
+
 
 def logInAsAdmin():
     mainCanvas.delete("all")
@@ -23,10 +23,45 @@ def logInAsAdmin():
     dbRow = cursor.fetchone()
     adminUser = Admin(dbRow[1],dbRow[2],dbRow[0])
     
+    def goSearch():
+        resultTable = ttk.Treeview(root)
+        resultTable['columns']=("CRN", "Title", "Day", "Credits", "Semester")
+        resultTable.column("#0", width=30)
+        resultTable.column("CRN", width=50)
+        resultTable.column("Title", width=200)
+        resultTable.column("Day", width=30)
+        resultTable.column("Credits", width=30)
+        resultTable.column("Semester", width=50)
+        
+        resultTable.heading("#0", text="Check")
+        resultTable.heading("CRN", text="CRN")
+        resultTable.heading("Title", text="Title")
+        resultTable.heading("Day", text="Day")
+        resultTable.heading("Credits", text="Credits")
+        resultTable.heading("Semester", text="Semester")
+        
+        
+        mainCanvas.create_window(50,300, window=resultTable)
+        
+        
+        
+        
+        
+        
+    
+    def search():
+        mainCanvas.delete("all")
+        mainCanvas.create_text(300, 30, text="Search Course by entering one of those param (any):",font=('Helvetica','14','bold'))
+        mainCanvas.create_text(300, 100, text="CRN, Time, Day, Title ",font=('Helvetica','11'))
+        mainCanvas.create_text(200, 140, text="Param: ",font=('Helvetica','11'))
+        mainCanvas.create_window(340, 140, window=searchEntry) 
+        goBttn = Button(root, text="Go", font=('Helvetica','11'), command=goSearch, width=10 )
+        mainCanvas.create_window(400, 140, window= goBttn)
+        
     mainCanvas.create_text(300, 30, text="Leopard Web accesed by: Admin "+ adminUser.firstName ,font=('Helvetica','14','bold'))
     mainCanvas.create_text(300, 100, text="Please select an item to continue: ",font=('Helvetica','11'))
     
-    searchAllBttn = Button(root, text="Search all Courses", font=('Helvetica','11'), command=Dummy, width=30 )
+    searchAllBttn = Button(root, text="Search all Courses", font=('Helvetica','11'), command=search, width=30 )
     mainCanvas.create_window(300, 150, window= searchAllBttn)
     searchParamBttn = Button(root, text="Search Courses by parameters", font=('Helvetica','11'), command=Dummy, width=30 )
     mainCanvas.create_window(300, 200, window= searchParamBttn)
@@ -42,14 +77,49 @@ def logInAsStudent():
     dbRow = cursor.fetchone()
     studentUser = Student(dbRow[1],dbRow[2],dbRow[0])
     
-    mainCanvas.create_text(300, 30, text="Leopard Web accesed by: "+ studentUser.firstName ,font=('Helvetica','14','bold'))
+    def goSearch():
+        resultTable = ttk.Treeview(root, columns=(0,1,2,3,4,5), show='headings', height=5)
+        resultTable.column(0, width=20)
+        resultTable.column(1, width=60)
+        resultTable.column(2, width=250)
+        resultTable.column(3, width=50)
+        resultTable.column(4, width=50)
+        resultTable.column(5, width=70)
+        
+        resultTable.heading(0, text=" ")
+        resultTable.heading(1, text="CRN")
+        resultTable.heading(2, text="Title")
+        resultTable.heading(3, text="Day")
+        resultTable.heading(4, text="Credits")
+        resultTable.heading(5, text="Semester")
+        
+        
+        mainCanvas.create_window(270,300, window=resultTable)
+        
+    
+    def search():
+        mainCanvas.delete("all")
+        mainCanvas.create_text(300, 30, text="Search Course by entering one of those param (any):",font=('Helvetica','14','bold'))
+        mainCanvas.create_text(300, 100, text="CRN, Time, Day, Title ",font=('Helvetica','11'))
+        mainCanvas.create_text(200, 140, text="Param: ",font=('Helvetica','11'))
+        mainCanvas.create_window(340, 140, window=searchEntry) 
+        goBttn = Button(root, text="Go", font=('Helvetica','11'), command=goSearch, width=20 )
+        mainCanvas.create_window(300, 200, window= goBttn)
+    
+    def addCourse():
+        mainCanvas.delete("all")
+        mainCanvas.create_text(300, 30, text="Add Course by entering CRN",font=('Helvetica','14','bold'))
+        mainCanvas.create_text(200, 100, text="CRN: ",font=('Helvetica','11'))
+        mainCanvas.create_window(340, 100, window=searchEntry) 
+        goBttn = Button(root, text="Go", font=('Helvetica','11'), command=goSearch, width=20 )
+        mainCanvas.create_window(300, 200, window= goBttn)
+        
+    mainCanvas.create_text(300, 30, text="Leopard Web accesed by: Admin "+ studentUser.firstName ,font=('Helvetica','14','bold'))
     mainCanvas.create_text(300, 100, text="Please select an item to continue: ",font=('Helvetica','11'))
     
-    searchAllBttn = Button(root, text="Search all Courses", font=('Helvetica','11'), command=Dummy, width=30 )
-    mainCanvas.create_window(300, 150, window= searchAllBttn)
-    searchParamBttn = Button(root, text="Search Courses by parameters", font=('Helvetica','11'), command=Dummy, width=30 )
-    mainCanvas.create_window(300, 200, window= searchParamBttn)
-    addCourseBttn = Button(root, text="Add Course to schedule", font=('Helvetica','11'), command=Dummy, width=30 )
+    searchBttn = Button(root, text="Search Courses", font=('Helvetica','11'), command=search, width=30 )
+    mainCanvas.create_window(300, 200, window= searchBttn)
+    addCourseBttn = Button(root, text="Add Course to schedule", font=('Helvetica','11'), command=addCourse, width=30 )
     mainCanvas.create_window(300, 250, window= addCourseBttn)
     removeCourseBttn = Button(root, text="Remove Course from schedule", font=('Helvetica','11'), command=Dummy, width=30 )
     mainCanvas.create_window(300, 300, window= removeCourseBttn)
@@ -61,13 +131,23 @@ def logInAsInstructor():
     dbRow = cursor.fetchone()
     instructorUser = Instructor(dbRow[1],dbRow[2],dbRow[0])
     
+    def goSearch():
+        pass
+    
+    def search():
+        mainCanvas.delete("all")
+        mainCanvas.create_text(300, 30, text="Search Course by entering one of those param (any):",font=('Helvetica','14','bold'))
+        mainCanvas.create_text(300, 100, text="CRN, Time, Day, Title ",font=('Helvetica','11'))
+        mainCanvas.create_text(200, 140, text="Param: ",font=('Helvetica','11'))
+        mainCanvas.create_window(340, 140, window=searchEntry) 
+        goBttn = Button(root, text="Go", font=('Helvetica','11'), command=goSearch, width=20 )
+        mainCanvas.create_window(300, 260, window= goBttn)
+        
     mainCanvas.create_text(300, 30, text="Leopard Web accesed by: Prof. "+ instructorUser.firstName ,font=('Helvetica','14','bold'))
     mainCanvas.create_text(300, 100, text="Please select an item to continue: ",font=('Helvetica','11'))
     
-    searchAllBttn = Button(root, text="Search all Courses", font=('Helvetica','11'), command=Dummy, width=30 )
-    mainCanvas.create_window(300, 150, window= searchAllBttn)
-    searchParamBttn = Button(root, text="Search Courses by parameters", font=('Helvetica','11'), command=Dummy, width=30 )
-    mainCanvas.create_window(300, 200, window= searchParamBttn)
+    searchBttn = Button(root, text="Search Courses", font=('Helvetica','11'), command=search, width=30 )
+    mainCanvas.create_window(300, 200, window= searchBttn)
     addRemoveBttn = Button(root, text="Print Course Roster", font=('Helvetica','11'), command=Dummy, width=30 )
     mainCanvas.create_window(300, 200, window= addRemoveBttn)
     
@@ -100,6 +180,7 @@ def main():
     root.geometry('600x500')
     root['padx']=20
     
+  
     global mainCanvas
     mainCanvas = Canvas(root, width=400, height=400) #create canvas
     mainCanvas.pack(fill="both", expand=True)
@@ -107,10 +188,11 @@ def main():
     mainCanvas.create_text(300, 30, text="Welcome to Leopard Web",font=('Helvetica','14','bold'))
     mainCanvas.create_text(300, 100, text="Please enter your Username and password. ",font=('Helvetica','11'))
     
-    global userIDEntry, passwordEntry
+    global userIDEntry, passwordEntry, searchEntry
     userIDEntry= Entry(root,font=('Helvetica','11'), width=20 ) 
     passwordEntry= Entry(root,show="*",font=('Helvetica','11'), width=20 ) 
-
+    searchEntry= Entry(root,font=('Helvetica','11'), width=20 ) 
+    
     mainCanvas.create_text(200, 140, text="Username: ",font=('Helvetica','11'))
     mainCanvas.create_window(340, 140, window=userIDEntry) 
     mainCanvas.create_text(200, 170, text="Password: ",font=('Helvetica','11'))
