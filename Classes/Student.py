164893caf95fc089823711183ddbd760c76e02a6
somaliJuice99 <78242555+somaliJuice99@users.noreply.github.com>
2022-07-08 21:39:07 -0400

    Update Student.py

--- Classes/Student.py
+++ Classes/Student.py
@@ -19,8 +19,19 @@ class Student(User):
 
     def addCourse(self, courseName):
        self.schedule.append(courseName)
+       #cursor.execute("""SELECT *  FROM COURSE WHERE CRN = ?""", (CRN))
+       #print(f"{self.firstName} added the course: {CRN} to his/her schedule.")
+       #query_result = cursor.fetchone()
+       #self.schedule.append(query_result[1])#add course title to the schedule list
+       # I, Abdullahi I. added these to the code. Yet to be finalized
 
     def dropCourse(self, courseName):
+       # cursor.execute("""SELECT *  FROM COURSE WHERE CRN = ?;""", (CRN))
+       # print(f"{self.firstName} dropped the course: {CRN} from his/her schedule.")
+       # query_result = cursor.fetchone()
+       # self.schedule.remove(query_result[1])#remove course title to the schedule list
+        # I, Abdullahi I. added these to the code. Yet to be finalized 
+
         self.schedule.remove(courseName)
 
 
