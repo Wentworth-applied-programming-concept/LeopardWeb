cac0d4e846b17a0f04edffa54ea189b0dedc5ede
somaliJuice99 <78242555+somaliJuice99@users.noreply.github.com>
2022-07-08 21:44:42 -0400

    Update Admin.py

--- Classes/Admin.py
+++ Classes/Admin.py
@@ -12,17 +12,55 @@ class Admin(User):
 
     def addCourseToSystem(self, CRN, title, department, time, day, semester, year, credits):
         cursor.execute("""INSERT OR IGNORE INTO COURSE (CRN, TITLE, DEPARTMENT, TIME, DAY, SEMESTER, YEAR, CREDITS ) VALUES (?,?,?,?,?,?,?,?) """, (CRN, title, department, time, day, semester, year, credits))
-
+       # print(f"You have added the course {TITLE} to the system.\n")
+       # cursor.execute("""INSERT INTO COURSE VALUES ?, ?, ?, ?, ?, ?, ?, ? """, (CRN, TITLE, DEPARTMENT, TIME, DAY, SEMESTER, YEAR, CREDITS))
+        #Abdul made edits here, stil in progress
+        
     def removeCourseFromSystem(self, CRNin):
         cursor.execute("""DELETE FROM COURSE WHERE CRN = ? """, (CRNin,))
-
+        #print(f"You have removed the course {TITLE} from the system.\n")
+        #cursor.execute("""DELETE FROM COURSE WHERE CRN or TITLE = ?, ? """, (CRN, TITLE))
+        #Abdul made edits here 
+        
     def addUser(self, firstName, lastName, ID, userType):
         print(f"You have add a new {userType} to the system\n")
 
+  #  def addStudentUser(self, ID, NAME, SURNAME, GRADYEAR, MAJOR, EMAIL):
+  #      print(f"You have add a new Student to the system\n")
+  #      cursor.execute("""INSERT INTO COURSE VALUES ?, ?, ?, ?, ?, ? """, (ID, NAME, SURNAME, GRADYEAR, MAJOR, EMAIL))
+
+  #  def addInstructorUser(self, ID, NAME, SURNAME, TITLE, HIREYEAR, DEPT, EMAIL):
+  #      print(f"You have add a new Instructor to the system\n")
+   #     cursor.execute("""INSERT INTO COURSE VALUES ?, ?, ?, ?, ?, ?, ? """, (ID, NAME, SURNAME, TITLE, HIREYEAR, DEPT, EMAIL))
+
+  #  def addAdminUser(self, ID, NAME, SURNAME, TITLE, OFFICE, EMAIL):
+  #      print(f"You have added a new admin to the system\n")
+   #     cursor.execute("""INSERT INTO COURSE VALUES ?, ?, ?, ?, ?, ? """, (ID, NAME, SURNAME, TITLE, OFFICE, EMAIL))
+   #Abdul made edits above with the comments
+        
+        
+        
     def removeUser(self, userID):
         print(
             f"You have removed the user with UserID: {userID} from the system\n")
 
+    #def removeStudentUser(self, ID):
+     #   print(f"You have deleted a Student from the system\n")
+      #  cursor.execute("""DELETE FROM STUDENT WHERE ID = ? """, (ID))
+
+   # def removeInstructorUser(self, ID):
+    #    print(f"You have deleted an instructor from the system\n")
+     #   cursor.execute("""DELETE FROM INSTRUCTOR WHERE ID = ? """, (ID))
+
+   # def removeAdminUser(self, ID):
+    #    print(f"You have deleted an admin from the system\n")
+     #   cursor.execute("""DELETE FROM ADMIN WHERE ID = ? """, (ID))
+   #Abdul made edits above here as well
+        
+        
+        
+        
+        
     def addStudentToCourse(self, studentID, courseName):
         print(f"You have added a new student to the course: {courseName}.\n")
 
