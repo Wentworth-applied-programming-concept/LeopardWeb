d0cf81c08694ed41b905be001555026f3b284adf
somaliJuice99 <78242555+somaliJuice99@users.noreply.github.com>
2022-07-14 14:42:14 -0400

    Added Test Cases

---
+++ test/test_admin.py
@@ -0,0 +1,13 @@
+import unittest
+from Classes.Admin import Admin
+
+#Create a test class for admin
+class Test_Admin(unittest.TestCase):
+    def test_addCourseToSystem(self):
+        pass
+
+    def test_removeCourseFromSystem(self):
+        pass
+
+    def test_searchCourse(self):
+        pass
---
+++ test/test_instructor.py
@@ -0,0 +1,14 @@
+import unittest
+from Classes.Instructor import Instructor
+
+#Creating a Test Class for the Instructor Class Functions
+class Test_Instructor(unittest.TestCase):
+    #def test_printSchedule(self):
+    #    pass
+
+    #def test_printClassList(self):
+    #    pass
+    
+    def test_searchCourse(self):
+        pass
+
